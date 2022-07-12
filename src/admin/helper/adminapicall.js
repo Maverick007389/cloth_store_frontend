d7d09df6e37f4f6b53f588a90cc51c35d0a900f6
Maverick007389 <mayankmalviya31089@gmail.com>
2022-07-12 07:23:57 +0530

    cards

--- src/admin/helper/adminapicall.js
+++ src/admin/helper/adminapicall.js
@@ -31,7 +31,7 @@ export const getCategories = () => {
 //products calls
 
 //create a product
-export const createaProduct = (userId, token, product) => {
+export const createProduct = (userId, token, product) => {
   return fetch(`${API}/product/create/${userId}`, {
     method: "POST",
     headers: {
--- src/core/Home.js
+++ src/core/Home.js
@@ -2,14 +2,16 @@ import React from "react";
 import { API } from "../backendconnect";
 import "../styles.css";
 import Base from "./Base";
+import Card from "./Card";
+
 
 export default function Home() {
   console.log("API IS", API);
   return (
     <Base title="Home Page" description="Welcome to the Cloth Store">
-      <div className="row">
+      <div className="row text-center">
         <div className="col-4">
-          <button className="btn btn-success">TEST</button>
+          <Card></Card>
         </div>
         <div className="col-4">
           <button className="btn btn-success">TEST</button>
